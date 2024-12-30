eb0c71dabbaac6ff8220a1b2066dd56d839932b2
NascuBB <2504vano@gmail.com>
2024-12-30 19:26:53 +0100

    начало обменника

---
+++ HomeSite/Controllers/FileshareController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeSite.Controllers
+{
+    public class FileshareController : Controller
+    {
+        // GET: FileshareController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //// GET: FileshareController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: FileshareController/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: FileshareController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: FileshareController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: FileshareController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: FileshareController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: FileshareController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ HomeSite/Helpers/FileShareManager.cs
@@ -0,0 +1,13 @@
+﻿namespace HomeSite.Helpers
+{
+    public static class FileShareManager
+    {
+        public static void PrepareFileShare()
+        {
+            if(!Path.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Share")))
+            {
+                Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "Share"));
+            }
+        }
+    }
+}
