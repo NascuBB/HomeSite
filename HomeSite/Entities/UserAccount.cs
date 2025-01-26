e9749cffe02ed72865c2f93bccc20037b71b7d59
NascuBB <2504vano@gmail.com>
2025-01-26 23:59:38 +0100

    Создал контекст и класс контрекста

---
+++ HomeSite/Entities/UserAccount.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeSite.Entities
+{
+	[Index(nameof(Email), IsUnique = true)]
+	[Index(nameof(Username), IsUnique = true)]
+	public class UserAccount
+	{
+		[Key]
+        public int Id { get; set; }
+		[MaxLength(20)]
+        public string Username { get; set; }
+		[MaxLength(20)]
+		public string Password { get; set; }
+		[MaxLength(20)]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
