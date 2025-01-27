5c1e444595f30053278494643024ccbceda6bc1c
NascuBB <2504vano@gmail.com>
2025-01-27 01:19:27 +0100

    половина контроллера акаунта

---
+++ HomeSite/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using HomeSite.Entities;
+using HomeSite.Helpers;
+using HomeSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeSite.Controllers
+{
+	public class AccountController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Register(RegisterViewModel model)
+		{
+			if(ModelState.IsValid)
+			{
+				Random rnd = new Random();
+				UserAccount newAccount = new UserAccount { Email = model.Email, Id = rnd.Next(1000,9999), Password = model.Password, PasswordHash = SecurePasswordHasher.Hash(model.Password) };
+			}
+			return View(model);
+		}
+	}
+}
