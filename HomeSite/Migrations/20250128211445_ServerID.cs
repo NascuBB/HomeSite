43405ee96bc16bb2dd9aa4437d748692e928ff25
NascuBB <2504vano@gmail.com>
2025-02-14 19:52:05 +0100

    миграции и гит игнор

---
+++ HomeSite/Migrations/20250128211445_ServerID.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeSite.Migrations
+{
+    /// <inheritdoc />
+    public partial class ServerID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ServerID",
+                table: "UserAccounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServerID",
+                table: "UserAccounts");
+        }
+    }
+}
